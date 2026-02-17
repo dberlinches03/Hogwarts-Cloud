8c33055924f83eeba8c71f876e3c9205345d8a4c
davberamo <dberlinches2003@gmail.com>
2026-02-17 15:42:30 +0100

    Creando el test para el servicio

---
+++ src/test/java/org/accesodatos/hogwarts/services/EstudianteServiceTest.java
@@ -0,0 +1,9 @@
+package org.accesodatos.hogwarts.services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class) // Para inicializar Mockito sin cargar Spring
+public class EstudianteServiceTest {
+
+}
